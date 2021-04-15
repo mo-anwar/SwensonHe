a7622b62957f4a3e8f52f6aaff0d37c545fc2206
Mohamed Anwar <mohamed.anwar@warbabank.com>
2021-04-15 20:06:58 +0200

    Initial Commit

---
+++ SwensonHeTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwensonHeTask
+//
+//  Created by Mohamed Anwar on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
